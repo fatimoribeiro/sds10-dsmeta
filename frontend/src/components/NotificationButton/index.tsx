deb3ffd695f77d271cf86ae85163bcd5891daf93
Fatimo Ribeiro <fatimoribeiro@gmail.com>
2022-09-11 11:52:18 -0300

    Send Notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,22 @@
 import './styles.css';
+import axios from 'axios';
 import icon from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from '../../utils/request';
 
-function NotificationButton() {
+type Props = {
+    saleId: number;
+}
+
+function handleClick(id : number) {
+    axios(`${BASE_URL}/sales/${id}/notification`)
+        .then(response => {
+            console.log("sucesso");
+        })
+}
+
+function NotificationButton( {saleId} : Props) {
     return (
-        <div className="dsmeta-notification-btn">
+        <div className="dsmeta-notification-btn" onClick={() => handleClick(saleId)}>
             <img src={icon} alt="Notificar" />
         </div>
     )
